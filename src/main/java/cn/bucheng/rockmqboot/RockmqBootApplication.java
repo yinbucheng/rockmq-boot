bae492198d084ed7c42476f8cf40f3600c7b4197
yinbucheng <yin.chong@intellif.com>
2019-07-24 08:56:38 +0800

    集成log4j2

--- src/main/java/cn/bucheng/rockmqboot/RockmqBootApplication.java
+++ src/main/java/cn/bucheng/rockmqboot/RockmqBootApplication.java
@@ -1,12 +1,15 @@
 package cn.bucheng.rockmqboot;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@Slf4j
 public class RockmqBootApplication {
 
     public static void main(String[] args) {
+        log.info("begain start application");
         SpringApplication.run(RockmqBootApplication.class, args);
     }
 
